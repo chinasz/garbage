b567b9fe3bb485d44f5ab3485b2762d1c43107d3
sz <1053700416@qq.com>
2019-05-22 11:36:58 +0800

    删除商城相关mobilecontroller

--- xinyuan_treasure/inc/mobile/exchange_center.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-	global $_W,$_GPC;
-	$_W['page']['sitename'] = '个人中心';
-	$query = load()->object('query');
-	$query->from($this->table['member'])->where(array('member_id'=>$_SESSION['ids']))->get();
-	
-	include $this->template('exchange_center');
--- xinyuan_treasure/inc/mobile/goods.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-	global $_W,$_GPC;
-	
-	include $this->template('exchange_goods');
